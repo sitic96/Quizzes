2ac0b4a8756fb2673c6c88e6606d9396509b2205
Sitora Guliamova <redastify@gmail.com>
2021-01-25 22:41:53 +0300

    main-screen: get rid of unused functionality

--- SoccerQuiz/SoccerQuiz/UI/Main menu/Quizes screen/QuizesViewModel.swift
+++ SoccerQuiz/SoccerQuiz/UI/Main menu/Quizes screen/QuizesViewModel.swift
@@ -10,18 +10,11 @@ import Foundation
 protocol QuizesViewModelProtocol {
     var appTitle: String { get }
     var pointsNumber: Int { get }
-    var showQuizzesViewState: ShowQuizzesViewState { get }
     var allQuizzes: [Quiz] { get }
-    var allWithoutFinishedQuizzes: [Quiz] { get }
-
-    var didToogleQuizzesState: (() -> Void)? { get set }
-
-    func didSelectToogleQuizzesViewState()
 }
 
 class QuizesViewModel {
     private let quizzes: [Quiz]
-    var _showQuizzesViewState: ShowQuizzesViewState = .hideFinished
 
     var didToogleQuizzesState: (() -> Void)?
 
@@ -39,30 +32,9 @@ extension QuizesViewModel: QuizesViewModelProtocol {
         5
     }
 
-    var showQuizzesViewState: ShowQuizzesViewState {
-        _showQuizzesViewState
-    }
-
     var allQuizzes: [Quiz] {
         quizzes.sorted(by: {
             $0.availabilityStatus < $1.availabilityStatus
         })
-    }
-
-    var allWithoutFinishedQuizzes: [Quiz] {
-        quizzes
-            .filter { $0.availabilityStatus != .finished }
-            .sorted(by: {
-                $0.availabilityStatus < $1.availabilityStatus
-            })
-    }
-
-    func didSelectToogleQuizzesViewState() {
-        if _showQuizzesViewState == .showAll {
-            _showQuizzesViewState = .hideFinished
-        } else if _showQuizzesViewState == .hideFinished {
-            _showQuizzesViewState = .showAll
-        }
-        didToogleQuizzesState?()
     }
 }
--- SoccerQuiz/SoccerQuiz/UI/Main menu/Quizes screen/QuizzesViewController.swift
+++ SoccerQuiz/SoccerQuiz/UI/Main menu/Quizes screen/QuizzesViewController.swift
@@ -18,32 +18,25 @@ class QuizzesViewController: UIViewController {
     @IBOutlet private weak var ballImageView: UIImageView!
     @IBOutlet private weak var ballsNumberLabel: UILabel!
     @IBOutlet private weak var quizzesCollectionView: UICollectionView!
-
+    
     var viewModel: QuizesViewModelProtocol!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindViewModel()
         setupStyle()
         setupView()
     }
-
-    private func bindViewModel() {
-        viewModel.didToogleQuizzesState = { [weak self] in
-            self?.quizzesCollectionView.reloadData()
-        }
-    }
-
+    
     private func setupStyle() {
         view.backgroundColor = StyleManager.MainScreen.backgroundColor
-
+        
         appTitleLabel.textColor = StyleManager.General.Colors.mainColor
         appTitleLabel.font = StyleManager.General.Fonts.titleFont
-
+        
         ballsNumberLabel.textColor = StyleManager.General.Colors.Text.mainColor
         ballsNumberLabel.font = StyleManager.General.Fonts.titleFont
     }
-
+    
     private func setupView() {
         appTitleLabel.text = viewModel.appTitle.localized()
         ballImageView.image = #imageLiteral(resourceName: "pointsImage.png")
@@ -55,7 +48,7 @@ class QuizzesViewController: UIViewController {
 
 extension QuizzesViewController: UICollectionViewDelegate, UICollectionViewDataSource,
                                  UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView,
                         viewForSupplementaryElementOfKind kind: String,
                         at indexPath: IndexPath) -> UICollectionReusableView {
@@ -71,16 +64,12 @@ extension QuizzesViewController: UICollectionViewDelegate, UICollectionViewDataS
                                 selectQuizTextColor: StyleManager.General.Colors.Text.mainColor)
         return reusableView
     }
-
+    
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        if viewModel.showQuizzesViewState == .hideFinished {
-            return viewModel.allWithoutFinishedQuizzes.count + Const.numberOfSpecialCells
-        } else {
-            return viewModel.allQuizzes.count + Const.numberOfSpecialCells
-        }
+        return viewModel.allQuizzes.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView
@@ -88,51 +77,27 @@ extension QuizzesViewController: UICollectionViewDelegate, UICollectionViewDataS
                                      for: indexPath) as? QuizCollectionViewCell else {
             return UICollectionViewCell()
         }
-        if indexPath.item == Const.indexOfSpecialCell {
-            cell.setupContent(image: #imageLiteral(resourceName: "logo_ball"), name: "Meow")
-            cell.setupStyle(titleLabelFont: StyleManager.General.Fonts.titleFont,
-                            titleLabelTextColor: StyleManager.General.Colors.Text.whiteColor)
-
-        } else {
-            if viewModel.showQuizzesViewState == .showAll {
-                cell.setupContent(image: #imageLiteral(resourceName: "pointsImage"), name: viewModel.allQuizzes[indexPath.item - Const.numberOfSpecialCells].title)
-                cell.setupStyle(titleLabelFont: StyleManager.General.Fonts.subtitleFont,
-                                titleLabelTextColor: StyleManager.General.Colors.Text.whiteColor)
-            } else if viewModel.showQuizzesViewState == .hideFinished {
-                cell.setupContent(image: #imageLiteral(resourceName: "pointsImage"),
-                                  name: viewModel.allWithoutFinishedQuizzes[indexPath.item -
-                                                                                Const.numberOfSpecialCells].title)
-                cell.setupStyle(titleLabelFont: StyleManager.General.Fonts.subtitleFont,
-                                titleLabelTextColor: StyleManager.General.Colors.Text.whiteColor)
-            }
-        }
+        cell.setupContent(image: #imageLiteral(resourceName: "pointsImage"), name: viewModel.allQuizzes[indexPath.item].title)
+        cell.setupStyle(titleLabelFont: StyleManager.General.Fonts.subtitleFont,
+                        titleLabelTextColor: StyleManager.General.Colors.Text.whiteColor)
         return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = (collectionView.frame.width - Const.quizzesCellSpacing) / 2
         return CGSize(width: width, height: width * Const.cellHeightToWidthRatio)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return Const.quizzesCellSpacing
     }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        didSelectItemAt indexPath: IndexPath) {
-        if indexPath.item == Const.indexOfSpecialCell {
-            viewModel.didSelectToogleQuizzesViewState()
-        }
-    }
 }
 
 fileprivate extension Const {
-    static let indexOfSpecialCell = 0
-    static let numberOfSpecialCells = 1
     static let cellHeightToWidthRatio: CGFloat = 0.8
     static let quizzesCellSpacing: CGFloat = 15.0
 }
